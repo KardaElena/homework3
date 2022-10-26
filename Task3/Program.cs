186b2c6e8ad7086a8f2606bcec5d1d6c72cd233b
KardaElena <karda.elena@gmail.com>
2022-10-26 13:29:09 +0300

    A tasa about weekdays was added

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Clear();
+
+Console.WriteLine("В этой программе вы можете узнать по номеру дня недели, является ли он выходным.");
+Console.WriteLine("\nВведите номер дня недели: ");
+
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+while (number < 1 || number > 7)
+{
+Console.WriteLine("Это не номер дня недели. Введите номер дня недели от 1 до 7: ");
+number = int.Parse(Console.ReadLine() ?? "0");
+}
+
+if (number == 6 || number == 7)
+Console.WriteLine("Это выходной день");
+
+else
+Console.WriteLine("Это будний день");
